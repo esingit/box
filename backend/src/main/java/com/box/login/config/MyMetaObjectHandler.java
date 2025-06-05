93111c70658b9b78aba304d416373030bf678d4d
“ESin” <291626442@qq.com>
2025-06-05 11:40:53 +0800

    基础功能+健身模块完成

--- backend/src/main/java/com/box/login/config/MyMetaObjectHandler.java
+++ backend/src/main/java/com/box/login/config/MyMetaObjectHandler.java
@@ -1,6 +1,7 @@
 package com.box.login.config;
 
 import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import com.box.login.entity.User;
 import org.apache.ibatis.reflection.MetaObject;
 import org.springframework.stereotype.Component;
 
@@ -25,11 +26,23 @@ public class MyMetaObjectHandler implements MetaObjectHandler {
         this.strictInsertFill(metaObject, "createTime", LocalDateTime.class, LocalDateTime.now());
         this.strictInsertFill(metaObject, "updateTime", LocalDateTime.class, LocalDateTime.now());
         this.strictInsertFill(metaObject, "deleted", Integer.class, 0);
-        String username = getCurrentUsername();
-        if (username != null) {
-            this.strictInsertFill(metaObject, "createUser", String.class, username);
-            this.strictInsertFill(metaObject, "updateUser", String.class, username);
+        
+        // 获取实体对象
+        String createUser = null;
+        if (metaObject.getOriginalObject() instanceof User) {
+            User user = (User) metaObject.getOriginalObject();
+            createUser = user.getUsername();  // 如果是用户注册，使用注册的用户名
+        } else {
+            createUser = getCurrentUsername();  // 其他情况使用当前用户名
         }
+        
+        // 如果还是null，使用默认值
+        if (createUser == null) {
+            createUser = "system";
+        }
+        
+        this.strictInsertFill(metaObject, "createUser", String.class, createUser);
+        this.strictInsertFill(metaObject, "updateUser", String.class, createUser);
     }
 
     @Override
