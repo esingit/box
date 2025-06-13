f60b0a0d6e8fd8b329ec56e0913a0ea5f11b59dc
“ESin” <291626442@qq.com>
2025-06-14 00:16:10 +0800

    完成

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,6 +4,25 @@ import router from '@/router'
 import {createPinia} from 'pinia'
 import {useUserStore} from '@/stores/userStore'
 import naive from 'naive-ui'
+import ECharts from 'vue-echarts'
+import { use } from 'echarts/core'
+
+// 按需引入 echarts 模块
+import {
+  CanvasRenderer
+} from 'echarts/renderers'
+import {
+  BarChart,
+  LineChart,
+  PieChart
+} from 'echarts/charts'
+import {
+  TitleComponent,
+  TooltipComponent,
+  LegendComponent,
+  GridComponent
+} from 'echarts/components'
+
 // 主题和基础样式
 import './assets/theme.css'
 import './assets/base.css'
@@ -25,12 +44,28 @@ import './assets/utilities.css'
 async function initApp() {
   const pinia = createPinia()
   const app = createApp(App)
+
   app.use(pinia)
+
   app.use(router)
   // 初始化store
   const userStore = useUserStore()
   await userStore.hydrate()
+
   app.use(naive)
+
+  use([
+    CanvasRenderer,
+    BarChart,
+    LineChart,
+    PieChart,
+    TitleComponent,
+    TooltipComponent,
+    LegendComponent,
+    GridComponent
+  ])
+  app.component('v-chart', ECharts)
+
   app.mount('#app')
 }
 
